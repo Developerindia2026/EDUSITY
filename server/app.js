19385e1ae716c1266a98a01b0a56b81d7623e31e
Developer2026 <developmentindia2026@gmail.com>
2026-06-26 23:13:28 +0530

    backend contact route fix

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const contactRoute = require("./routes/contact");
+const contactRoute = require("./routes/Contact");
 
 const app = express();
 
